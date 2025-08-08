17ff34cf9220074d3e5ff23236f1863feb8f0a67
Kenesu-Zoro <kennethdcosta29@gmail.com>
2025-08-08 13:13:55 +0530

    refactor: reorganize MongoDB connection and improve product API routes

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected!")
+        app.listen(PORT, () => {
+            console.log(`Serving on port : ${PORT}`) // synatx : ${x} it should be inside `` not '' or ""
+        })
+    })
+    .catch(() => {
+        console.error("Something went wrong when connecting to the Database !")
+    })
+
+    
 app.get('/', (req, res) => {
+    
     res.send("hello there mathafakas!")
 })
+
 
 app.post('/api/v1/product', async (req, res) => {
     try {
@@ -33,14 +47,31 @@ app.get('/api/v1/get-product', async (req, res) => {
     }
 })
 
+app.get('/api/v1/get-product/:id', async (req, res) => { 
+   try {
+     const { id } = req.params;
+     const productID = await Product.findById(id)
+     res.status(200).json(productID)
+   } catch (error) {
+        res.status(500).json({message: error.message})
+   }
+})
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB connected!")
-        app.listen(PORT, () => {
-            console.log(`Serving on port : ${PORT}`) // synatx : ${x} it should be inside `` not '' or ""
-        })
-    })
-    .catch(() => {
-        console.error("Something went wrong when connecting to the Database !")
-    })
+app.put('/api/v1/update-product/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body)
+
+        if(!product){
+            res.status(401).json({message:"No product found!"})
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+         res.status(500).json({message: error.message})
+    }
+})
+
+
+
